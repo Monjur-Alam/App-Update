98a072c4b54d83cf9a2a8a539242921e35c64c09
Munjur <munjur.alam@periscopelabs.io>
2025-06-11 21:49:29 +0600

    Initial Commit

---
+++ AppUpdate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppUpdate
+//
+//  Created by MunjurAlam on 11/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppUpdateTests/AppUpdateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppUpdateTests.swift
+//  AppUpdateTests
+//
+//  Created by MunjurAlam on 11/6/25.
+//
+
+import Testing
+@testable import AppUpdate
+
+struct AppUpdateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
